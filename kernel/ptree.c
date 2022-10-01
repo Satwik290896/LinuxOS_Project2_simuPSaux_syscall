95a0722fa7ea912740bd6f5899d8791258d94d7e
Mohsin Rizvi <mkr2151@columbia.edu>
2022-10-01 08:51:39 -0400

    clean up ptree.c and add more error checking

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -11,19 +11,19 @@
 static struct task_struct *get_root(int root_pid)
 {
 	if (root_pid == 0)
-			return &init_task;
+		return &init_task;
 
 	return find_task_by_vpid(root_pid);
 }
 
-void assignBufferValue(struct prinfo *buf, struct task_struct *p, int *count, int level){
+/* populate an entry in buf with the info from task_struct p. */
+void assignBufferValue(struct prinfo *buf, struct task_struct *p, int *count, int level)
+{
 	buf[*count].pid = p->pid;
 	buf[*count].parent_pid = p->real_parent->pid;
 	buf[*count].uid = (int)p->real_cred->uid.val;
 	buf[*count].level = level;
 	strcpy(buf[*count].comm, p->comm);
-	/* printk("%d, %d, %d, %d, %s\n\n", buf[*count].pid, buf[*count].parent_pid, buf[*count].uid, level, buf[*count].comm);
-	printk("%d\n", *count); */
 	(*count)++;
 }
 
@@ -41,6 +41,10 @@ SYSCALL_DEFINE3(ptree, struct prinfo __user *, buf, int __user *, nr, int, root_
 	if (buf == NULL || nr == NULL)
 		return -EINVAL;
 
+	if (root_task == NULL) {
+		return -ESRCH;
+	}
+
 	/* copy *nr from user space into max_entries */
 	if (copy_from_user(&max_entries, nr, sizeof(int)))
 		return -EFAULT;
@@ -48,34 +52,26 @@ SYSCALL_DEFINE3(ptree, struct prinfo __user *, buf, int __user *, nr, int, root_
 	if (max_entries < 1)
 		return -EINVAL;
 
-	/* TODO: allocate a buffer for up to 'max_entries' prinfo entries.
-	 * the total space needed will be (max_entries * sizeof(struct prinfo)).
-	 */
 	buffer = kmalloc(max_entries * sizeof(struct prinfo), GFP_KERNEL);
-	if(!buffer)
-		return -EFAULT;
-	for(i = 0; i < max_entries; i++)
+	if (!buffer)
+		return -ENOMEM;
+	for (i = 0; i < max_entries; i++)
 		buffer[i].pid = -1;
 
 	rcu_read_lock();
 
-	/* TODO: from root_task, do an iterative BFS.
-	 * for each task_struct that we iterate on:
-	 *   populate a prinfo entry, store it in our allocated buffer at index [actual_entries]
-	 *   increment actual_entries
-	 *   if actual_entries == max_entries, stop iterating.
-	 * be sure not to do anything blocking (like allocation) in this section
-	 * between rcu_read_lock and rcu_read_unlock :)
-	 */
 	p = root_task;
 	assignBufferValue(buffer, p, &actual_entries, 0);
 
-	while(buffer[buf_q].pid >= 0 && actual_entries < max_entries){
-		// printk("parent pid: %d", buffer[buf_q].pid);
+	/* do our process BFS:
+	 * for each process in buffer (of up to *nr processes):
+	 *   for each of its child processes:
+	 *     add the child into buffer, for up to *nr total processes
+	 */
+	while (buffer[buf_q].pid >= 0 && actual_entries < max_entries) {
 		p = get_root(buffer[buf_q].pid);
-		list_for_each(list, &p->children){
-			if(actual_entries >= max_entries)
-				break;
+		list_for_each(list, &p->children) {
+			if (actual_entries >= max_entries) break;
 			task = list_entry(list, struct task_struct, sibling);
 			assignBufferValue(buffer, task, &actual_entries, buffer[buf_q].level + 1);
 		}
@@ -84,20 +80,14 @@ SYSCALL_DEFINE3(ptree, struct prinfo __user *, buf, int __user *, nr, int, root_
 	
 	rcu_read_unlock();
 
-    /* TODO: copy our stored prinfo entries into *buf using copy_to_user.
-	 *  if this fails, return -EFAULT
-	 * then, delete our allocated buffer
-	 */
+	/* copy stored prinfo entries into buf, and update *nr. */
 	if(copy_to_user(buf, buffer, actual_entries * sizeof(struct prinfo)))
 		return -EFAULT;
 
-	/* update *nr with how many entries were copied */
 	if (copy_to_user(nr, &actual_entries, sizeof(int)))
 		return -EFAULT;
+
 	kfree(buffer);
-	/* delete this later, this is just to silence unused variable warnings for now */
-	(void) root_task;
-	(void) actual_entries;
 
 	return 0;
 }
