de10a82dd4c65adb536da8eb7a87c90612791b4a
TigerCWShaw <cs4213@columbia.edu>
2022-10-01 05:43:19 -0400

    implement get current process's children

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -3,21 +3,46 @@
 #include <linux/sched/task.h>
 #include <linux/prinfo.h>
 #include <linux/syscalls.h>
+#include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/string.h>
+#include <linux/unistd.h>
 
 static struct task_struct *get_root(int root_pid)
 {
-        if (root_pid == 0)
-                return &init_task;
+	if (root_pid == 0)
+			return &init_task;
 
-        return find_task_by_vpid(root_pid);
+	return find_task_by_vpid(root_pid);
 }
+
+void assignBufferValue(struct prinfo *buf, struct task_struct *p, int *count){
+	buf[*count].pid = p->pid;
+	buf[*count].parent_pid = p->real_parent->pid;
+	buf[*count].uid = (int)p->real_cred->uid.val;
+	strcpy(buf[*count].comm, p->comm);
+	printk("%d,%d,%d,%s\n\n", buf[*count].pid, buf[*count].parent_pid, buf[*count].uid, buf[*count].comm);
+	printk("%d\n", *count);
+	(*count)++;
+}
+
+struct ptask{
+	int pid;
+	int level;
+	struct list_head *list;
+};
 
 SYSCALL_DEFINE3(ptree, struct prinfo __user *, buf, int __user *, nr, int, root_pid)
 {
 	struct task_struct *root_task = get_root(root_pid);
 	int max_entries;
 	int actual_entries = 0;
+	struct prinfo *buffer;
+	struct task_struct *p, *task;
+	int count = 0;
+	struct list_head *list;
 
+	printk("Hello world from systemcall\n");
 	if (buf == NULL || nr == NULL)
 		return -EINVAL;
 
@@ -31,6 +56,10 @@ SYSCALL_DEFINE3(ptree, struct prinfo __user *, buf, int __user *, nr, int, root_
 	/* TODO: allocate a buffer for up to 'max_entries' prinfo entries.
 	 * the total space needed will be (max_entries * sizeof(struct prinfo)).
 	 */
+	buffer = kmalloc(max_entries * sizeof(struct prinfo), GFP_KERNEL);
+	if(!buffer)
+		return -EFAULT;
+	
 
 	rcu_read_lock();
 
@@ -42,6 +71,16 @@ SYSCALL_DEFINE3(ptree, struct prinfo __user *, buf, int __user *, nr, int, root_
 	 * be sure not to do anything blocking (like allocation) in this section
 	 * between rcu_read_lock and rcu_read_unlock :)
 	 */
+	p = root_task;
+	buffer[count].level = 0;
+	assignBufferValue(buffer, p, &count);
+
+	list_for_each(list, &p->children){
+		if(count >= max_entries)
+			break;
+		task = list_entry(list, struct task_struct, sibling);
+		assignBufferValue(buffer, task, &count);
+	}
 	
 	rcu_read_unlock();
 
@@ -53,11 +92,11 @@ SYSCALL_DEFINE3(ptree, struct prinfo __user *, buf, int __user *, nr, int, root_
 	/* update *nr with how many entries were copied */
 	if (copy_to_user(nr, &actual_entries, sizeof(int)))
 		return -EFAULT;
-	
+	kfree(buffer);
 	/* delete this later, this is just to silence unused variable warnings for now */
 	(void) root_task;
 	(void) actual_entries;
-	
+
 	return 0;
 }
 
